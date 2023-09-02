59bb4e8a6396afa128896a5b32946cfda4627997
francisputong <francisputong01@gmail.com>
2023-09-02 21:53:06 +0800

    db setup and config

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+type Props = {};
+
+const InitialModal = (props: Props) => {
+    return <div>this will be a modal</div>;
+};
+
+export default InitialModal;
