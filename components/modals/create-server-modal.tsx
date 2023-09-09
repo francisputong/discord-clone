19e0d7fd8ac5b5b06ee9ede7270ac9398fbd7631
francisputong <francisputong01@gmail.com>
2023-09-09 14:33:41 +0800

    add create server modal

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import * as z from 'zod';
@@ -18,6 +17,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { FileUpload } from '@/components/file-upload';
+import { useModal } from '@/hooks/use-modal-store';
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -28,11 +28,11 @@ const formSchema = z.object({
     })
 });
 
-const InitialModal = () => {
-    const [isMounted, setIsMounted] = useState(false);
+const CreateServerModal = () => {
+    const { isOpen, onClose, type } = useModal();
     const router = useRouter();
 
-    useEffect(() => setIsMounted(true), []);
+    const isModalOpen = isOpen && type === 'createServer';
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -49,16 +49,19 @@ const InitialModal = () => {
             await axios.post('/api/servers', values);
             form.reset();
             router.refresh();
-            window.location.reload();
+            onClose();
         } catch (error) {
             console.log(error);
         }
     };
 
-    if (!isMounted) return null;
+    const handleClose = () => {
+        form.reset();
+        onClose();
+    };
 
     return (
-        <Dialog open>
+        <Dialog open={isModalOpen} onOpenChange={handleClose}>
             <DialogContent className='bg-white text-black p-0 overflow-hidden'>
                 <DialogHeader className='pt-8 px-6'>
                     <DialogTitle className='text-2xl text-center font-bold'>Customize your server</DialogTitle>
@@ -123,4 +126,4 @@ const InitialModal = () => {
     );
 };
 
-export default InitialModal;
+export default CreateServerModal;
--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -2,14 +2,17 @@
 
 import { Plus } from 'lucide-react';
 import ActionTooltip from '@/components/action-tooltip';
+import { useModal } from '@/hooks/use-modal-store';
 
 type Props = {};
 
 const NavigationAction = (props: Props) => {
+    const { onOpen } = useModal();
+
     return (
         <div>
             <ActionTooltip side='right' align='center' label='Add a server'>
-                <button className='group'>
+                <button onClick={() => onOpen('createServer')} className='group flex items-center'>
                     <div className='mx-3 flex h-[48px] w-[48px] items-center justify-center overflow-hidden rounded-[24px] bg-background transition-all group-hover:rounded-[16px] group-hover:bg-emerald-500 dark:bg-neutral-700'>
                         <Plus className='text-emerald-500 transition group-hover:text-white' />
                     </div>
